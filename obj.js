14effc4d7fb42f0a392ad0fc5d41db88035121f4
Clare Zhu <clarexzhu@gmail.com>
2019-01-15 21:39:16 -0800

    Ensure NN meshes are loaded before beginning visualization

--- obj.js
+++ obj.js
@@ -27,32 +27,52 @@ function loadObj(objf, mtlf) {
     return contain
 }
 
+function promisifyLoader ( loader ) {
+
+  function promiseLoader ( url ) {
+    return new Promise( ( resolve, reject ) => {
+      loader.load( url, resolve, Function.prototype, reject );
+    } );
+  }
+
+  return {
+    originalLoader: loader,
+    load: promiseLoader,
+  };
+
+}
+
 function loadNN(color) {
     var objf = 'resources/nn.obj';
     var mtlf = 'resources/nn.mtl';
     var contain = new THREE.Object3D();
     var obj;
 
-    function onMTLLoad( materials ) {
-        materials.preload();
+	function onOBJLoad( object ) {
+	   obj = object;
+	   obj.scale.x = 50;
+	   obj.scale.y = 50;
+	   obj.scale.z = 50;
+	   obj.children[0].material.color.setHex(parseInt(color.substr(1), 16));
+	   contain.add(obj);
+       return new Promise( ( resolve, reject ) => {
+          resolve( { myColor: color, obj: contain} );
+       } ); // ugly but gets the job done
+	}
 
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials( materials );
+	function onMTLLoad( materials ) {
+		materials.preload();
 
-        function onOBJLoad(object) {
-           obj = object;
-           obj.scale.x = 50;
-           obj.scale.y = 50;
-           obj.scale.z = 50;
-           obj.children[0].material.color.setHex(parseInt(color.substr(1), 16));
+		var objLoader = new THREE.OBJLoader();
+		objLoader.setMaterials(materials);
+		var OBJPromiseLoader = promisifyLoader(objLoader);
+		var objPromise = OBJPromiseLoader.load(objf);
+		return objPromise;
+	}
 
-           contain.add(obj)
-        }
-        objLoader.load( objf, onOBJLoad);
-    }
-
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.load( mtlf, onMTLLoad);
-    return contain
+	var MTLPromiseLoader = promisifyLoader(new THREE.MTLLoader());
+	var mtlPromise = MTLPromiseLoader.load(mtlf);
+	var finalPromise = mtlPromise.then(materials=>onMTLLoad(materials))
+			                     .then(object=>onOBJLoad(object));
+	return finalPromise;
 }
-
