6982575217171f33658951227c1fa6d6a6247d98
jsuarez5341 <sealsuarez@gmail.com>
2019-02-18 19:26:18 -0800

    Refactor server, fix kill bug

--- twistedserver.py
+++ twistedserver.py
@@ -1,10 +1,9 @@
 from pdb import set_trace as T
 import numpy as np
 
-import sys
-import json
+from signal import signal, SIGINT
+import sys, os, json, pickle
 import ray
-import pickle
 
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
@@ -14,7 +13,6 @@ from twisted.web.static import File
 
 from autobahn.twisted.websocket import WebSocketServerFactory, \
     WebSocketServerProtocol
-
 from autobahn.twisted.resource import WebSocketResource
 
 def sign(x):
@@ -32,47 +30,31 @@ def move(orig, targ):
     else:
         return ro + sign(dr), co + sign(dc)
 
-class EchoServerProtocol(WebSocketServerProtocol):
+class GodswordServerProtocol(WebSocketServerProtocol):
     def __init__(self):
         super().__init__()
-        print("CREATED A SERVER")
+        print("Created a server")
         self.frame = 0
         self.packet = {}
 
-    def visVals(self, vals, sz):
-      ary = np.zeros((sz, sz, 3))
-      vMean = np.mean([e[1] for e in vals])
-      vStd  = np.std([e[1] for e in vals])
-      nStd, nTol = 4.0, 0.5
-      grayVal = int(255 / nStd * nTol)
-      for v in vals:
-         pos, mat = v
-         r, c = pos
-         mat = (mat - vMean) / vStd
-         color = np.clip(mat, -nStd, nStd)
-         color = int(color * 255.0 / nStd)
-         if color > 0:
-             color = (0, color, 128)
-         else:
-             color = (-color, 0, 128)
-         ary[r, c] = color
-      return ary.astype(np.uint8)
-
     def onOpen(self):
         print("Opened connection to server")
-        #packet = json.dumps(data).encode('utf8')
-        #self.sendMessage(packet, True)
 
     def onClose(self, wasClean, code=None, reason=None):
         print('Connection closed')
 
-    def serverPacket(self):
-        data = self.realm.clientData.remote()
-        data = ray.get(data)
-        data = pickle.loads(data)
-        return data
- 
-    #Correct connection method?
+    def connectionMade(self):
+        super().connectionMade()
+        self.factory.clientConnectionMade(self)
+
+    def connectionLost(self, reason):
+        super().connectionLost(reason)
+        self.factory.clientConnectionLost(self)
+
+    #Not used without player interaction
+    def onMessage(self, packet, isBinary):
+        print("Message", packet)
+
     def onConnect(self, request):
         print("WebSocket connection request: {}".format(request))
         realm = self.factory.realm.envs[0]
@@ -88,46 +70,17 @@ class EchoServerProtocol(WebSocketServerProtocol):
 
         self.sendUpdate()
 
-    def onMessage(self, packet, isBinary):
-        print("Message", packet)
-
-        packet = json.loads(packet)
-        #self.sendMessage(payload, isBinary)
-        #packet = packet['0']
-
-        #pos = packet['pos']
-        #ent = self.packet['ent']
-        #ent['0'] = {'pos': move(ent['0']['pos'], pos)}
-
+    def serverPacket(self):
+        data = self.realm.clientData.remote()
+        data = ray.get(data)
+        data = pickle.loads(data)
+        return data
 
     def sendUpdate(self):
         ent = {}
         data = self.serverPacket()
         entities = data['entities']
         environment = data['environment']
-        '''
-        for id, e in entities.items():
-           pkt = {}
-           pkt['pos']  = e.pos
-           pkt['entID'] = e.entID
-           pkt['color'] = e.color.hex
-           pkt['name'] = e.name
-           pkt['food'] = e.food.val
-           pkt['water'] = e.water.val
-           pkt['health'] = e.health.val
-           pkt['maxFood'] = e.food.max
-           pkt['maxWater'] = e.water.max
-           pkt['maxHealth'] = e.health.max
-           pkt['damage'] = e.damage
-           pkt['attackMap'] = e.attackMap
-
-           pkt['attack'] = None
-           pkt['target'] = None
-           if e.attack is not None: 
-              pkt['attack'] = e.attack.action.__name__
-              pkt['target'] = e.attack.args.entID
-           ent[id] = pkt
-        '''
         self.packet['ent'] = entities
 
         gameMap = environment.np().tolist()
@@ -148,13 +101,26 @@ class EchoServerProtocol(WebSocketServerProtocol):
         packet = json.dumps(self.packet).encode('utf8')
         self.sendMessage(packet, False)
 
-    def connectionMade(self):
-        super().connectionMade()
-        self.factory.clientConnectionMade(self)
-
-    def connectionLost(self, reason):
-        super().connectionLost(reason)
-        self.factory.clientConnectionLost(self)
+    #Todo: would be nicer to move this into the javascript,
+    #But it would possibly have to go straight into the shader
+    def visVals(self, vals, sz):
+      ary = np.zeros((sz, sz, 3))
+      vMean = np.mean([e[1] for e in vals])
+      vStd  = np.std([e[1] for e in vals])
+      nStd, nTol = 4.0, 0.5
+      grayVal = int(255 / nStd * nTol)
+      for v in vals:
+         pos, mat = v
+         r, c = pos
+         mat = (mat - vMean) / vStd
+         color = np.clip(mat, -nStd, nStd)
+         color = int(color * 255.0 / nStd)
+         if color > 0:
+             color = (0, color, 128)
+         else:
+             color = (-color, 0, 128)
+         ary[r, c] = color
+      return ary.astype(np.uint8)
 
 class WSServerFactory(WebSocketServerFactory):
     def __init__(self, ip, realm, step):
@@ -184,30 +150,28 @@ class WSServerFactory(WebSocketServerFactory):
         self.clients.remove(client)
 
 class Application:
-    def __init__(self, realm, step):
-        self.realm = realm
-        #log.startLogging(sys.stdout)
-        port = 8080
+   def __init__(self, realm, step):
+      signal(SIGINT, self.kill)
+      self.realm = realm
+      #log.startLogging(sys.stdout)
+      port = 8080
 
-        #factory = WSServerFactory(u'ws://localhost:'+str(port), realm)
-        factory = WSServerFactory(u"ws://127.0.0.1:8080", realm, step)
-        #factory = WebSocketServerFactory(u"ws://127.0.0.1:8080")
-        factory.protocol = EchoServerProtocol
+      factory = WSServerFactory(u'ws://localhost:' + str(port), realm, step)
+      factory.protocol = GodswordServerProtocol 
+      resource         = WebSocketResource(factory)
 
-        resource = WebSocketResource(factory)
+      # We server static files under "/" and our WebSocket 
+      # server under "/ws" (note that Twisted uses bytes 
+      # for URIs) under one Twisted Web Site
+      root = File(".")
+      root.putChild(b"ws", resource)
+      site = Site(root)
 
-        # we server static files under "/" ..
-        root = File(".")
+      reactor.listenTCP(port, site)
+      reactor.run()
 
-        # and our WebSocket server under "/ws" (note that Twisted uses
-        # bytes for URIs)
-        root.putChild(b"ws", resource)
-
-        # both under one Twisted Web Site
-        site = Site(root)
-
-        reactor.listenTCP(port, site)
-        reactor.run()
-
-
+   def kill(*args):
+      print("Killed by user")
+      reactor.stop()
+      os._exit(0)
 
