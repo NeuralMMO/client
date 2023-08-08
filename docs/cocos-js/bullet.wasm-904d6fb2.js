cb1b52529070b4e6f840fd0c25e44db8da0c1b6c
jsuarez5341 <sealsuarez@gmail.com>
2023-08-07 17:41:00 -0800

    That didn't work

--- docs/cocos-js/bullet.wasm-904d6fb2.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e,t){"use strict";return{execute:function(){e("default",new URL("assets/bullet.wasm-c98527b6.wasm",t.meta.url).href)}}}));
--- docs/cocos-js/wait-for-ammo-instantiation.js
+++
@@ -1,1 +0,0 @@
-System.register(["./instantiated-cbc35ae6.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hw)}],execute:function(){}}}));
